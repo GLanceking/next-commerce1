336c95c62491baa0a3c2a91dc059e2814793130d
GLanceking <sakusen@naver.com>
2023-01-01 16:04:37 +0900

    이미지 슬라이드 test

--- pages/products2.tsx
+++ pages/products2.tsx
@@ -1,5 +1,7 @@
+import Head from 'next/head'
 import Image from 'next/image'
 import Carousel from 'nuka-carousel/lib/carousel'
+import { useState } from 'react'
 
 const images = [
   {
@@ -26,20 +28,60 @@ const images = [
     original: 'https://picsum.photos/id/1019/1000/600/',
     thumbnail: 'https://picsum.photos/id/1019/250/150/',
   },
+  {
+    original:
+      'https://raw.githubusercontent.com/xiaolin/react-image-gallery/master/static/6.jpg',
+    thumbnail:
+      'https://raw.githubusercontent.com/xiaolin/react-image-gallery/master/static/6.jpg',
+  },
 ]
 
 export default function Products2() {
+  const [imgIndex, setImgIndex] = useState(0)
   return (
-    <Carousel animation="fade">
-      {images.map((item) => (
-        <Image
-          key={item.original}
-          src={item.original}
-          alt="testpage-img"
-          width={1000}
-          height={600}
-        />
-      ))}
-    </Carousel>
+    <>
+      <Head>
+        <title>이미지 슬라이드</title>
+      </Head>
+      <div
+        style={{
+          width: 800,
+          height: 450,
+          border: 'solid pink',
+          position: 'relative',
+          overflow: 'hidden',
+        }}
+      >
+        <Carousel withoutControls slideIndex={imgIndex}>
+          {images.map((item) => (
+            <Image
+              key={item.original}
+              src={item.original}
+              alt="testpage-img"
+              width={800}
+              height={1}
+            />
+          ))}
+        </Carousel>
+      </div>
+      <div style={{ display: 'flex' }}>
+        {images.map((item, imgesIndex) => (
+          <div
+            key={imgesIndex}
+            onClick={() => {
+              setImgIndex(imgesIndex)
+            }}
+          >
+            <Image
+              key={item.thumbnail}
+              src={item.thumbnail}
+              alt="thumb-img"
+              width={100}
+              height={50}
+            />
+          </div>
+        ))}
+      </div>
+    </>
   )
 }
